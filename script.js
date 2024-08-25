5436d1373e9900c02e6841e370529ab5cada490c
Denis-Hish <hish.denis@gmail.com>
2024-08-25 09:39:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const dialog = document.querySelector('dialog');
+const showDialog = document.querySelector('button');
+const closeDialog = dialog.querySelector('button');
+
+showDialog.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeDialog.addEventListener('click', () => {
+  dialog.close();
+});
